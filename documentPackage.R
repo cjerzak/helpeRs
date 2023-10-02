3d0d9db9c4a10092c70e3f2bb9a79ba8237f6ea4
cjerzak <connor.jerzak@gmail.com>
2023-10-02 10:48:07 -0500

    up

--- documentPackage.R
+++ documentPackage.R
@@ -6,6 +6,9 @@ devtools::document(package_path)
 try(file.remove(sprintf("./helpeRs.pdf")),T)
 system(paste(shQuote(file.path(R.home("bin"), "R")), "CMD", "Rd2pdf", shQuote(package_path)))
 
+# Check package to ensure it meets CRAN standards.
+devtools::check( package_path )
+
 #install.packages(package_path)
 
 #install.packages( "~/Documents/helpeRs-software/helpeRs",repos = NULL, type = "source")
