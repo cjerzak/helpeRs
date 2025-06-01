a69134ebc84eb7c11f494dfd031a37e51a77c40b
cjerzak <connor.jerzak@gmail.com>
2025-06-01 11:26:01 -0400

    up

--- helpeRs/R/helpeRs_Stargazer2FullTable.R
+++ helpeRs/R/helpeRs_Stargazer2FullTable.R
@@ -1,4 +1,3 @@
-
 #' Implements...
 #'
 #' @usage
@@ -23,11 +22,28 @@
 #'
 #' @md
 
+#' Convert a stargazer table to a self-contained longtable.
+#'
+#' @param stargazer_text A character vector – the raw lines produced by
+#'   `stargazer()` (as captured with `capture.output()` or the browser() trick).
+#' @param fontsize       LaTeX font-size environment to wrap the table in
+#'                       (e.g. "footnotesize", "scriptsize").
+#' @param continued_note Logical.  If TRUE (default) add the
+#'                       "(Continued from previous page)" banner that longtable
+#'                       prints *only* on page ≥ 2.  Set to FALSE if you do not
+#'                       want that banner at all.
+#'
+#' @return A character vector ready to be `cat()`-ed into a .tex file.
+#' @export
+#'
+#' @examples
+#' tex_lines <- Stargazer2FullTable(stargazer_text)
+#' cat(tex_lines, file = "table.tex", sep = "\n")
+#' 
 Stargazer2FullTable <- function(stargazer_text, fontsize = "footnotesize"){
-  browser()
   stargazer_text_orig <- stargazer_text
-  stargazer_text = sapply(stargazer_text,function(sa)gsub(sa,pattern="\\[!htbp\\]",replace=""))
-  stargazer_text = sapply(stargazer_text,function(sa)gsub(sa,pattern="\\\\centering",replace=""))
+  stargazer_text = sapply(stargazer_text,function(sa)gsub(sa,pattern="\\[!htbp\\]",replace=""), USE.NAMES = FALSE)
+  stargazer_text = sapply(stargazer_text,function(sa)gsub(sa,pattern="\\\\centering",replace=""), USE.NAMES = FALSE)
   col_arrange <- stargazer_text[grepl(stargazer_text,pattern = "\\\\begin\\{tabular\\}")]
   pattern <- "\\\\begin\\{tabular\\}\\{.*?\\\\extracolsep\\{5pt\\}\\}\\s+(\\w+)"
   col_arrange <- sub(pattern, "\\1", col_arrange)
@@ -36,23 +52,27 @@ Stargazer2FullTable <- function(stargazer_text, fontsize = "footnotesize"){
   stargazer_text = sapply(stargazer_text,function(sa){gsub(sa, pattern="\\\\begin\\{table\\}",
                                                            replace= sprintf("\\\\begin\\{%s\\}
                                                             \\\\begin\\{longtable\\}\\{%s\\}",
-                                                                       fontsize, col_arrange))})
+                                                                       fontsize, col_arrange))}, USE.NAMES = FALSE)
   stargazer_text = sapply(stargazer_text,function(sa){ gsub(sa,
                                                           pattern="\\\\end\\{table\\}",
                                                           replace=sprintf("\\\\end\\{longtable\\}
                                                             \\\\end\\{%s\\}", fontsize)
-                                                          ) } )
+                                                          ) } , USE.NAMES = FALSE)
   # see:
   # https://tex.stackexchange.com/questions/71549/how-can-i-repeat-the-header-but-not-the-caption-with-longtable
   stargazer_text  <- stargazer_text[!grepl(stargazer_text,pattern = "\\\\begin\\{tabular\\}")]
-  endhead_index <- grep(stargazer_text,pattern="\\&")[1]+1
-  stargazer_text <- c(stargazer_text[1:endhead_index],
-                      "\\endfirsthead",
-                      sprintf("\\multicolumn{%s}{c}",nchar(col_arrange)),
-                      "{\\it (Continued from previous page)} \\\\",
-                      stargazer_text[(endhead_index-3):endhead_index],
-                      "\\endhead",
-                      stargazer_text[(endhead_index+1):length(stargazer_text)])
+  if(length(stargazer_text) > 40){ 
+    endhead_index <- grep(stargazer_text,pattern="\\&")[1]+1
+      stargazer_text <- c(stargazer_text[1:endhead_index],
+                          "\\endfirsthead",
+                          sprintf("\\multicolumn{%s}{c}",nchar(col_arrange)),
+                          "{\\it (Continued from previous page)} \\\\",
+                          stargazer_text[(endhead_index-3):endhead_index],
+                          "\\endhead",
+                          stargazer_text[(endhead_index+1):length(stargazer_text)])
+  }
+  stargazer_text <- gsub(stargazer_text, x = "C L I P- R S I C D", replace = "CLIP-RSICD", USE.NAMES = F)
+  stargazer_text <- gsub(stargazer_text, x = "Vi T", replace = "ViT", USE.NAMES = F)
   stargazer_text <- sapply(stargazer_text,function(sa){ gsub(sa,pattern="\\\\end\\{tabular\\}", replace="") })
   return(  stargazer_text )
 }
