3f23748d77d625128029f6c98d0fea348f1b589a
cjerzak <connor.jerzak@gmail.com>
2026-06-01 08:45:37 -0400

    Ignore generated R artifacts

--- helpeRs.Rcheck/00_pkg_src/helpeRs/tests/testthat.R
+++
@@ -1,7 +0,0 @@
-# This file is the main test runner for the helpeRs package
-# Run all tests with: devtools::test() or testthat::test_dir("tests/testthat")
-
-library(testthat)
-library(helpeRs)
-
-test_check("helpeRs")
